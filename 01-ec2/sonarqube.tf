f690d19eb4c12a5e05693cb52ac4d54060807675
Rishi <1990rishi@gmail.com>
2019-10-20 18:28:06 +0530

    --- Auto-Git Commit ---

---
+++ 01-ec2/sonarqube.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  ami           = "ami-00c4b877294e28e09"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0b60b597fc467671d","sg-06e97f31248fda0ad"]
+  iam_instance_profile  = "EC2-RO-ROLE"
+  key_name              = "devops"
+
+  tags = {
+    Name = "sonarqube"
+  }
+}
