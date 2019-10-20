92ace36809dd373691cd007795a4de69e2248935
Rishi <1990rishi@gmail.com>
2019-10-20 19:14:42 +0530

    --- Auto-Git Commit ---

---
+++ basic/count/count.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  count = 2 
+  ami           = "ami-02383135f96b2293e"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-09c918b8c00442b89"]
+  iam_instance_profile  = "fulladmin"
+  key_name              = "RishiNVerginia"
+
+  tags = {
+    Name = "Server ${count.index}"
+}
